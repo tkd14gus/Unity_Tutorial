c8b28ca494533334d1f4b5d6cd3aa9780cb7ecd4
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-23 18:59:44 +0900

    유니티 튜토리얼 06
    카메라 : 카메라에 대한 속성과 스크립트를 이용해 그것들을 변경하는 법을 익혔다. 실습 중 왠지 모르지만 카메라가 정명을 바라보는 것이 아니라 뒤에서 앞을 바라고 있다는
                  느낌을 받았다. 왜인지 알아봐야 할 것 같다.

---
+++ Unity_Tutorial/Assets/Scripts/Test06.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test06 : MonoBehaviour
+{
+    ////(카메라가) 따라갈 타겟
+    //[SerializeField]
+    //private GameObject go_Target;
+    //
+    ////속도
+    //[SerializeField]
+    //private float speed;
+    //
+    ////거리
+    //private Vector3 difValue;
+
+    private Camera theCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ////거리 계산
+        //difValue = transform.position - go_Target.transform.position;
+        ////음수가 나올수도 있으니 절대값 해서 양수로
+        //difValue = new Vector3(-Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), -Mathf.Abs(difValue.z));
+
+        //시점 설정
+        theCam.fieldOfView = 50;
+        //flag변경
+        //theCam.clearFlags
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////Lerp = 움직임 보관법 -> 중간 값 계산
+        //this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, speed);
+    }
+}
