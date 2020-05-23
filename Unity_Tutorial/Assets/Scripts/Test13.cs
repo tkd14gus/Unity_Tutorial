204918eff18f0022bd0b50ff338c51733ae34cb6
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 01:42:08 +0900

    유니티 튜토리얼 13
    인풋 필드 UI : 간단하게 입력을 받을 수 있는 인풋 필드를 익혔다.

---
+++ Unity_Tutorial/Assets/Scripts/Test13.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test13 : MonoBehaviour
+{
+
+    [SerializeField] private Text txt_Money;
+    [SerializeField] private InputField inputTxT_Money;
+
+    private int currentMoney;
+
+    public void Input()
+    {
+        currentMoney += int.Parse(inputTxT_Money.text);
+
+        txt_Money.text = currentMoney.ToString();
+    }
+
+    public void Output()
+    {
+        currentMoney -= int.Parse(inputTxT_Money.text);
+
+        txt_Money.text = currentMoney.ToString();
+    }
+}
+    
+
