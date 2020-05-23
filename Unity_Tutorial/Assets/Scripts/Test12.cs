457f8c115d3a621c194ddbc613210c581867ed08
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 01:26:00 +0900

    유니티 튜토리얼 12
    슬라이더 UI : 슬라이더 UI를 사용하는 법을 익혔다.

---
+++ Unity_Tutorial/Assets/Scripts/Test12.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test12 : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+
+    private bool isClick;
+    private float dotTime = 1f;
+    private float currentDotTime = 0f;
+
+    void Update()
+    {
+
+        if (isClick)
+        {
+            currentDotTime -= Time.deltaTime;
+            if(currentDotTime <= 0)
+            {
+                slider.value -= Time.deltaTime;
+
+                if(currentDotTime <= -1f)
+                {
+                    currentDotTime = dotTime;
+                }
+            }
+        }
+        
+    }
+    public void Button()
+    {
+        //slider.value -= 1;
+        isClick = true;
+    }
+}
