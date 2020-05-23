1135c810880b5a49835afa1a58637200b5303bfa
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-23 23:26:55 +0900

    유니티 튜토리얼 10
    파티클 시스템 : 파티클 시스템이 무엇인지, 그리고 속성은 어느 것이 있는지, 마지막으로 스크립트를 이용한 간단한 함수를 알아보았다.

---
+++ Unity_Tutorial/Assets/Scripts/Test10.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test10 : MonoBehaviour
+{
+    ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //재생
+        ps.Play();
+        //정지
+        ps.Stop();
+        //일시정지
+        ps.Pause();
+        //순간 몇개가 뿜어져 나올지
+        ps.Emit(1);
+    }
+}
