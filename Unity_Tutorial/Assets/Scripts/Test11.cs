0dafc3170cfba00874c4dd1d6ddc3689492dd3fb
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 00:57:17 +0900

    유니티 튜토리얼 11
    UI의 기본 : UI중 버튼 캠퍼스, 이미지의 사용법을 익혔다.

---
+++ Unity_Tutorial/Assets/Scripts/Test11.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test11 : MonoBehaviour
+{
+
+    [SerializeField] private Text txt_Name;
+    [SerializeField] private Image img_Name;
+
+    //스프라이트 직접 넣기
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+
+    void Update()
+    {
+
+        //속성
+        //이미지 변경
+        //img_Name.sprite = 
+        //색
+        img_Name.color = Color.red;
+        //색에 맞춰 이미지 사라지게 하기
+        Color color = img_Name.color;
+        color.a = 0f;
+        img_Name.color = color;
+
+        if (isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_Name.fillAmount = currentTime / delayTime;
+
+            if(currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_Name.fillAmount = currentTime;
+            }
+        }
+    }
+    public void Change()
+    {
+        txt_Name.text = "변경됨";
+        //img_Name.fillAmount = 0.5f;
+        isCoolTime = true;
+    }
+}
