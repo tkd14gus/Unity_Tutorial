76d908bbe0809b5ee98175483b5dc78f5faed10d
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 22:50:48 +0900

    유니티 튜토리얼 25
    모바일 D 패드 : 모바일에서 사용되는 방향키를 구현하는 법을 배웠다. 하지만 아직 방향키가 중심에서 멀어질수록 빨리가는 부분에 대한 이해가 살짝 부족하다. 더 공부할 필요성이 느껴진다.

---
+++ Unity_Tutorial/Assets/Scripts/Test25.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Test25 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+
+    [SerializeField] private RectTransform rect_Background;
+    [SerializeField] private RectTransform rect_Joystick;
+
+    private float radius;
+
+    [SerializeField] private GameObject go_Player;
+    [SerializeField] private float moveSpeed;
+
+    private bool isTouch = false;
+    private Vector3 movePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = rect_Background.rect.width * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouch)
+            go_Player.transform.position += movePosition;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 value = eventData.position - (Vector2)rect_Background.position;
+
+        //가두기
+        value = Vector2.ClampMagnitude(value, radius);
+        //(1, 4) -> 빼고 더한만큼 자신을 가둠 -> (-3 ~ 5)
+        
+        rect_Joystick.localPosition = value;
+
+        float distance = Vector2.Distance(rect_Background.position, rect_Joystick.position) / radius;
+        value = value.normalized;   // 예 => (50, 0, 30) -> (0.5, 0.3) 방향빼고 속도만
+        movePosition = new Vector3(value.x * distance * moveSpeed * Time.deltaTime, 0f, value.y * distance * moveSpeed * Time.deltaTime);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTouch = false;
+        rect_Joystick.localPosition = Vector3.zero;
+        movePosition = Vector3.zero;
+    }
+}
