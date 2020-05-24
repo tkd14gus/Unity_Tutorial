fff616aa50fac7e3e6d7d2cf28d04eba60e08bf9
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 15:44:00 +0900

    유니티 튜토리얼 16
    오디오 소스 : 오디오 소스의 속성과 스크립트를 이용하여 사용하는 방법을 익혔다.

---
+++ Unity_Tutorial/Assets/Scripts/Test16.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test16 : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySE()
+    {
+        int _temp = Random.Range(0, 4);
+
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+    }
+    
+}
