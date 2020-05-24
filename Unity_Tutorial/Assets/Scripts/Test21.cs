6833367bceaaccb356b6f6c3c0c7c6045fee5458
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 21:08:02 +0900

    유니티 튜토리얼 21
    경로의 거리 계산 : 두 오브젝트간의 거리를 계산하는 법을 익혔다. 하지만 NavMeshPath에 대한 공부가 더 필요하다 판단된다.

---
+++ Unity_Tutorial/Assets/Scripts/Test21.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test21 : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, 10f);
+
+        if(col.Length > 0)
+        {
+            for (int i = 0; i < col.Length; i++)
+            {
+                Transform tf_Targer = col[i].transform;
+
+                if(tf_Targer.name == "Player")
+                {
+                    NavMeshPath path = new NavMeshPath();
+                    agent.CalculatePath(tf_Targer.position, path);
+
+                    Vector3[] wayPoint = new Vector3[path.corners.Length + 2];
+                    wayPoint[0] = transform.position;
+                    wayPoint[wayPoint.Length - 1] = tf_Targer.position;
+
+                    float _distance = 0f;
+                    for (int p = 0; p < path.corners.Length; p++)
+                    {
+                        wayPoint[p + 1] = path.corners[p];
+                        _distance = Vector3.Distance(wayPoint[p], wayPoint[p + 1]);
+                    }
+
+                    if(_distance <= 10f)
+                    {
+                        agent.SetDestination(tf_Targer.position);
+                    }
+                }
+            }
+        }
+    }
+}
