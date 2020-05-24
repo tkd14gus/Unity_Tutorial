1ef2df508166a7bd3271794f744f46069cb5a2d0
tkd14gus <qkrtkdgus78@gmail.com>
2020-05-24 21:43:08 +0900

    유니티 튜토리얼 23
    드래그&드롭 : Canvas안에서 드래그 드롭을 사용하는 법을 익혔다.

---
+++ Unity_Tutorial/Assets/Scripts/Test23.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+//이 클릭&드래그 인터페이스는
+//Canvas안에 있는 UI요소만 적용된다.
+//마우스를 때면 무조건  
+public class Test23 : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler //드래그 중일 때 그 객체에서 마우스가 벗어나지 않은 상태에서 클릭을 때면(조건이 존재_
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        Debug.Log("Drag");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drop");
+    }
+}
